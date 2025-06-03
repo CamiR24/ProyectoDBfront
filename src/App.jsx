b46197a79e7b7d143d82cf60ea991f323dde5599
Camila Richter <ric23183@uvg.edu.gt>
2025-06-02 20:54:17 -0600

    esqueleto de las páginas

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Patients          from './pages/Patients';
+import Especializations          from './pages/Especializations';
+import Doctors        from './pages/Doctors';
+import Labs         from './pages/Labs';
+import Results          from './pages/Results';
+import Appointments          from './pages/Appointments';
+import Hospitalizations          from './pages/Hospitalizations';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          {/* ---------- Públicas ---------- */}
+          <Route path="/"      element={<Patients />} />
+          <Route path="/especializations" element={<Especializations />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/labs" element={<Labs />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/hospitalizations" element={<Hospitalizations />} />
+
+          {/* Catch-all */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Se obtiene el elemento raíz definido en el index.html y se renderiza la aplicación.
+// React.StrictMode es opcional, ayuda a detectar problemas potenciales.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
